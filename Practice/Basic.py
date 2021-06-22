4f16f563f12ed3311e83517d174f0078510c9edb
dark-glich <mohdbilal9808464636@gmail.com>
2021-06-22 14:49:30 +0530

    Initialized different types of array

--- Practice/Basic.py
+++ Practice/Basic.py
@@ -27,7 +27,7 @@ print(f"vector.nbytes = {vector.nbytes} ") # 6 bytes
 # Changing and Accessing elements
 
 array = np.array([[1,2,3,4,5],[6,7,8,9,10]])
-print(array)
+print(f"array = {array}")
 
 # Accessing & changing a specific element
 print(f"array[1,1] = {array[1,-1]} ") # 7
@@ -43,3 +43,24 @@ array[:, 2] = [3, 88]
 
 # Accessing a secific part
 print(f"array[0, 1:-1:2] = {array[0, 1:-1:2]}") # [2 4]
+
+# Initializing different types of array
+
+# Creates a array full of zeroes 
+zero_array = np.zeros((3,3)) # np.zeros(shape)
+
+# Create a array full of ones
+ones_array = np.ones((2,2)) 
+
+# Create a array full of a specific value
+value_array = np.full((2,3), 2) # np.full(shape, value)
+
+# Random decimal array
+decimal_array = np.random.rand((3))
+
+# Random integer array 
+int_array = np.random.randint(4,10, size=(2,2)) # np.random.randint(min,max,size)
+print(int_array)
+
+# identity array => create a matrix with same number of columns & rows
+identity_array = np.identity(4) # np.identity(row/column)
