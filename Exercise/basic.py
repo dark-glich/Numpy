8b636f142e6894b53bdf6dd5554bb44b48d312b7
dark-glich <mohdbilal9808464636@gmail.com>
2021-06-24 14:46:11 +0530

    completed basic exercises

--- Exercise/basic.py
+++ Exercise/basic.py
@@ -57,5 +57,38 @@ p = np.arange(10, 22)
 p = p.reshape((3,4))
 print(p)
 
-# Write a NumPy program to create a 10x10 matrix, in which the elements on the borders will be equal to 1, and inside 0.
+# Write a NumPy program to create a 10x10 matrix, in which the elements on the borders will be equal to 0, and inside 1.
+z = np.zeros((10,10))
+o = np.ones((8,8))
+z[1:-1, 1:-1] = o
+print(z)
 
+# Write a NumPy program to create a 5x5 matrix with 6 & elements on the main diagonal equal to 1, 2, 3, 4, 5.
+s= np.full((5,5), 6)
+d = np.diag([1-6,2-6,3-6,4-6,5-6])
+print(s+d)
+
+# Write a NumPy program to create a 3x3x3 array filled with arbitrary values.
+r = np.random.random((3,3,3))
+print(r)
+
+# Write a NumPy program to compute sum of all elements, sum of each column and sum of each row of a given array.
+array = np.array([[10,20],[30,40]])
+print(array.sum())
+print(array.sum(axis=0))
+print(array.sum(axis=1))
+
+# Write a NumPy program to compute the inner product of two given vectors.
+x = np.array([2, 4])
+y = np.array([1, 3])
+print(np.dot(x,y))
+
+# Multiply two given arrays of same size element-by-element
+x = np.array([1,5,7,8,2])
+y = np.array([4,7,3,6,4])
+print(x*y)
+
+# Write a NumPy program to create an array of equal shape and data type of a given array.
+ex = np.array([[1,5],[8,2]])
+a = np.zeros_like(ex)
+print(a.dtype, a.shape)
